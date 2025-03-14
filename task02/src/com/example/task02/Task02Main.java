1f02b595a2792c9bc6187786c8801d5ffd131014
dimonodekolon <daluch@mail.ru>
2025-03-14 20:12:17 +0500

    task 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,8 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +11,22 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        int cur = System.in.read();
+        int next;
+
+        while (cur != -1) {
+            next = System.in.read();
+
+            if (cur == 13 && next == 10) {
+                cur = next;
+                continue;
+            }
+
+            System.out.write(cur);
+
+            cur = next;
+        }
+
+        System.out.flush();
     }
 }
