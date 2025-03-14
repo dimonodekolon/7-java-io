1c72ded755e4265aa9075e9f128d27c846ff5785
dimonodekolon <daluch@mail.ru>
2025-03-14 20:12:51 +0500

    task 4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,14 +1,31 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
+        InputStream inputStream = System.in;
+        double result = 0;
 
-        System.out.println("0.0");
+        InputStreamReader reader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        while (bufferedReader.ready()) {
+            String[] chunks = bufferedReader.readLine().split(" ");
+            for (String chunk : chunks) {
+                double num = 0;
+                try {
+                    num = Double.parseDouble(chunk);
+                } catch (RuntimeException e) {
+                }
+                result += num;
+            }
+        }
+        System.out.printf(Locale.US, "%.6f", result);
     }
 }
